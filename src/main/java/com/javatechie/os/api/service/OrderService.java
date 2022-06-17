8fd3640dba1be009889ae21640a84a367636c8cf
rey.limosenero <rey.limosenero@softvision.com>
2022-06-17 12:29:51 +0800

    original rest

--- src/main/java/com/javatechie/os/api/service/OrderService.java
+++ src/main/java/com/javatechie/os/api/service/OrderService.java
@@ -27,8 +27,6 @@ public class OrderService {
     @Lazy
     private RestTemplate template;
 
-/*    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
-    private String ENDPOINT_URL;*/
 
     public TransactionResponse saveOrder(TransactionRequest request) throws JsonProcessingException {
         String response = "";
@@ -39,10 +37,6 @@ public class OrderService {
         //rest call
         logger.info("Order-Service Request : "+new ObjectMapper().writeValueAsString(request));
 
-        //registered in API Gateway
-//TODO:        Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
-       // Payment paymentResponse = template.postForObject("http://localhost:9191/payment/doPayment", payment, Payment.class);
-        //registered with eureka
         Payment paymentResponse = template.postForObject("http://payment-service/payment/doPayment", payment, Payment.class);
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api , order added to cart";
         logger.info("Order Service getting Response from Payment-Service : "+new ObjectMapper().writeValueAsString(response));
